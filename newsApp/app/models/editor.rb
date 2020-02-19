e0ac3917dc2bf2233348c886dc8b7af5e059c7cd
Lauren Bannon <laurenbannon98@gmail.com>
2020-02-19 18:59:26 +0000

    Attempt at adding tests

--- newsApp/app/models/editor.rb
+++ newsApp/app/models/editor.rb
@@ -1,4 +1,8 @@
 class Editor < ApplicationRecord
+
+  validates :email, presence: true, length: {minimum: 10}
+  validates :password, presence: true, length: {minimum: 6}
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- newsApp/test/models/user_test.rb
+++ newsApp/test/models/user_test.rb
@@ -4,4 +4,14 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save user without email" do
+    user = User.new
+    assert_not user.save
+  end
+
+  test "should not save user with same email as another user" do
+    user = User.new
+    assert_not user.save
+  end
 end
