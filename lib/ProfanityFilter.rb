6fe887ccb427b88f0fee15330f0ab9f06b768439
Lauren Bannon <laurenbannon98@gmail.com>
2020-02-27 21:03:37 +0000

    Updating gem

--- lib/ProfanityFilter.rb
+++ lib/ProfanityFilter.rb
@@ -4,7 +4,7 @@ class ProfanityFilter
 
     downcase = input.downcase
 
-    swear_words = {'fuck' => 'f**k', 'bitch' => 'b**ch', 'cunt' => 'c**t', 'gee' => 'g**',
+    swear_words = {'fuck' => 'f**k', 'feck' => 'f**k', 'bitch' => 'b**ch', 'cunt' => 'c**t', 'gee' => 'g**',
                    'wanker' => 'w*nk*r', 'gimp' => 'g**p', 'shit' => 's**t', 'bastard' => 'b*s**rd',
                    'bollox' => 'b*l**x', 'dickhead' => 'd**kh**d', 'wank' => 'w**k', 'shithead' => 's**th**d',
                    'pussy' => 'p**sy', 'retard' => 'r*t**d', 'scumbag' => 's**mb*g',
@@ -20,20 +20,3 @@ class ProfanityFilter
   end
 
 end
-
-
-#puts "Input your sentence here: "
-#
-#text = gets.downcase.strip
-#
-#swear_words = {'fuck' => 'f**k', 'bitch' => 'b**ch', 'cunt' => 'c**t', 'gee' => 'g**',
-#               'wanker' => 'w*nk*r', 'gimp' => 'g**p', 'shit' => 's**t', 'bastard' => 'b*s**rd',
-#               'bollox' => 'b*l**x', 'dickhead' => 'd**kh**d', 'wank' => 'w**k', 'shithead' => 's**th**d',
-#               'pussy' => 'p**sy', 'retard' => 'r*t**d', 'scumbag' => 's**mb*g',
-#               'fag' => 'f**', 'faggot' => 'f*g**t', 'dyke' => 'd**e', 'scum' => 's**m'}
-#
-#swear_words.each do |word, clean|  # No need to copy here
-#  text.gsub!(word,clean)  # Changed from gsub
-#end
-#
-#puts text  # Changed from clean_text
